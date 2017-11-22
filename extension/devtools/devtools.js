5cac17b6a7c5c9aa997eb6bf39acb9e81bb40970
TheAdnan <adnankicin92@gmail.com>
2017-11-22 16:56:24 +0100

    manifest permission added

---
+++ extension/devtools/devtools.js
@@ -0,0 +1,19 @@
+function handleShown() {
+  console.log("panel is being shown");
+}
+
+function handleHidden() {
+  console.log("panel is being hidden");
+}
+
+/**
+Create a panel, and add listeners for panel show/hide events.
+*/
+browser.devtools.panels.create(
+  "Webscraper Dzup",
+  "icons/icon.png",
+  "devtools/panel/panel.html"
+).then((newPanel) => {
+  newPanel.onShown.addListener(handleShown);
+  newPanel.onHidden.addListener(handleHidden);
+}); 
---
+++ extension/devtools/panel/devtools-panel.js
@@ -0,0 +1,1 @@
+devtools-panel.js
